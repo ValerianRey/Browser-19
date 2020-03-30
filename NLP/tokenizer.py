25450a1811c90229904f07eb9933df8d2c656360
valerian.rey@gmail.com <valerian.rey@gmail.com>
2020-03-30 06:24:49 +0200

    improved tokenizer, improved clustering

--- NLP/tokenizer.py
+++ NLP/tokenizer.py
@@ -3,7 +3,7 @@ import os
 import re
 
 
-def clean_and_tokenize(text):
+def clean_and_tokenize(text, remove_numbers=False):
     """
     Cleaning a document with:
         - Lowercase
@@ -19,12 +19,14 @@ def clean_and_tokenize(text):
     # Lowercase
     text = text.lower()
     # Remove numbers
-    text = re.sub(r"[0-9]+", "", text)
+    if remove_numbers:
+        text = re.sub(r"[0-9]+", "", text)
     # Remove punctuation
-    remove_punct = re.compile("[.;:!\'?,\"()\[\]]")
+    remove_punct = re.compile("[.;:!\'?,\"()\[\]|]")
     text = remove_punct.sub("", text)
     # Remove HTML artifacts specific to the corpus we're going to work with
-    replace_html = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+    replace_html = re.compile("(<br\s*/><br\s*/>)|(\/)")  # do not remove the hyphen so that we keep for example: covid-19
+    # replace_html = re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
     text = replace_html.sub(" ", text)
 
     tokens = text.split()
